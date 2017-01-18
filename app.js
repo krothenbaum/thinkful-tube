3f5264a1a7f95f131abedbb643ba739e59cca82a
Kyle Rothenbaum <krothenbaum@gmail.com>
2017-01-18 14:33:03 -0800

    Introduced state management

--- app.js
+++ app.js
@@ -1,85 +1,92 @@
-var YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
-var KEY = 'AIzaSyA3IHL73MF00WFjgxdwzg57nI1CwW4dybQ';
-var USERSEARCHTERM = '';
-
-function getDataFromApi(searchTerm, callback) {
-  var query = {
-  	part: 'snippet',
-    key: KEY,
-    maxResults: 6,
-    type: 'video',
-    q: searchTerm
-  }
-  $.getJSON(YOUTUBE_URL, query, callback);
-}
-
-function getNextPageData(searchTerm, nextPage, callback) {
-  var query = {
+var state = {
+  query: {
     part: 'snippet',
-    key: KEY,
+    key: 'AIzaSyA3IHL73MF00WFjgxdwzg57nI1CwW4dybQ',
     maxResults: 6,
-    pageToken: nextPage,
+    pageToken: '',
     type: 'video',
-    q: searchTerm
-  }
-  $.getJSON(YOUTUBE_URL, query, callback);
+    q: ''
+  },
+  youtubeUrl: 'https://www.googleapis.com/youtube/v3/search',
+};
+
+//API call
+function getDataFromApi(state, callback) {
+  $.getJSON(state.youtubeUrl, state.query, callback);
 }
 
-function getPrevPageData(searchTerm, nextPage, callback) {
-  var query = {
-    part: 'snippet',
-    key: KEY,
-    maxResults: 6,
-    pageToken: nextPage,
-    type: 'video',
-    q: searchTerm
-  }
-  $.getJSON(YOUTUBE_URL, query, callback);
+//state management functions
+function setSearchTerm(state, element) {
+  state.query.q = element.val();
 }
 
-function displayYoutubeSearchData(data) {
+function setNextPageToken (state, data) {
+  state.nextPageToken = data.nextPageToken;
+}
+
+function setPrevPageToken (state, data) {
+  state.query.nextPage = data.prevPageToken;
+}
+
+//render functions
+function renderPage(data) {
+  renderResults(data);
+  renderButtons(state, data);
+}
+
+function renderResults(data) {
   var resultsHTML = '<div class="row">';
   data.items.forEach(function (item) {
     resultsHTML = (resultsHTML + '<div class="search-item col s12 m6"><div class="card small hoverable">' +
-            '<div class="card-image">' +
-              '<iframe src="https://www.youtube.com/embed/' + item.id.videoId + '" frameborder="0" allowfullscreen></iframe>' +
-            '</div>' +
-            '<div class="card-content">' +
-              '<p class="truncate">' + item.snippet.title + '</p>' +
-            '</div>' +
-            '<div class="card-action">' +
-              '<a href="https://www.youtube.com/channel/' + item.snippet.channelId + '">View more from '+ item.snippet.channelTitle + '</a>' +
-            '</div>' + 
-          '</div></div>')
-  });
-  
+      '<div class="card-image">' +
+        '<iframe src="https://www.youtube.com/embed/' + item.id.videoId + '" frameborder="0" allowfullscreen></iframe>' +
+      '</div>' +
+      '<div class="card-content">' +
+        '<p class="truncate">' + item.snippet.title + '</p>' +
+      '</div>' +
+      '<div class="card-action">' +
+        '<a href="https://www.youtube.com/channel/' + item.snippet.channelId + '">View more from '+ item.snippet.channelTitle + '</a>' +
+      '</div>' + 
+      '</div></div>')
+  });  
   resultsHTML = resultsHTML + '</div>';
-
   $('.search-results').html(resultsHTML);
+}
 
-  console.log(data.nextPageToken);
+function renderButtons(state, data) {
+  renderNextPageButton(state, data);
+  if (data.prevPageToken) {
+    renderPrevPageButton(state, data);
+  }
+}
+
+function renderNextPageButton(state, data) {
   $('.next-page').removeClass('hidden');
   $('.next-page').click(function(e) {
     e.preventDefault();
-    getNextPageData(USERSEARCHTERM, data.nextPageToken, displayYoutubeSearchData);
-    $('.prev-page').removeClass('hidden');
-    $('.prev-page').click(function(e) {
-      e.preventDefault();
-      getNextPageData(USERSEARCHTERM, data.prevPageToken, displayYoutubeSearchData);
-    });
+    state.query.pageToken = data.nextPageToken;
+    getDataFromApi(state, renderPage);
   });
-
-
 }
 
+function renderPrevPageButton(state, data) {
+  $('.prev-page').removeClass('hidden');
+  $('.prev-page').click(function(e) {
+    e.preventDefault();
+    state.query.pageToken = data.prevPageToken;
+    getDataFromApi(state, renderPage);
+  })
+}
+
+//event listener
 function watchSubmit() {
   $('form[name="user-search"]').submit(function(e) {
     e.preventDefault();
-    USERSEARCHTERM = $(this).find('.user-search-input').val();
-    getDataFromApi(USERSEARCHTERM, displayYoutubeSearchData); 
+    setSearchTerm(state, $(this).find('.user-search-input'));
+    getDataFromApi(state, renderPage); 
   });
 }
 
 $(document).ready(function () {
-	watchSubmit();
+	watchSubmit();  
 })
